aee5d51628c533f8da69b92bacd054be0952a155
Arsen Papiyan <papiyan.arsen98@gmail.com>
2022-04-04 18:33:27 +0400

    deploy

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
 import { createStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./rootReducer";
 
-const store = createStore(rootReducer)
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 store.subscribe(() => {
   const state = store.getState()
